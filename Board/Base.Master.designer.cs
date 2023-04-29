511da067a0c434369d1d85f5a3bf0ffe971ae1c5
nao-777 <119158647+Nao-777@users.noreply.github.com>
2023-04-29 18:38:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Board/Base.Master.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、正しくない動作の原因になる可能性があり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace Board.WebSite
+{
+
+
+    public partial class Base
+    {
+
+        /// <summary>
+        /// head コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// headerUser コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder headerUser;
+
+        /// <summary>
+        /// ContentPlaceHolder1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// cInputForm コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cInputForm;
+
+        /// <summary>
+        /// cMainDisplay コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cMainDisplay;
+    }
+}
---
+++ Board/WebSite/Form/Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Base.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Board.WebSite.Form.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server"></asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        ログインIDを入力してください
+            <br />
+        <asp:TextBox ID="tbLoginID" runat="server" />
+        <br />
+        <br />
+        パスワードを入力してください
+            <br />
+        <asp:TextBox ID="tbPasswordID" runat="server" />
+        <br />
+        <div>
+            <p id="pAlertMessage" runat="server"></p>
+        </div>
+        <br />
+        <asp:LinkButton ID="lbLogin" runat="server" OnClick="lbLogin_Click" Text="ログインボタン" />
+        <br />
+        <br />
+        <!--<a href="">新規登録の方はこちら</a> -->
+        <br />
+    </div>
+</asp:Content>
